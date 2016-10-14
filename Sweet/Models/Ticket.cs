51a45e903d6114f8b1a1a1a302fd5f35567ae395
Ryan Dixon <dixon104@marshall.edu>
2016-10-14 02:11:59 -0400

    new commit

---
+++ Sweet/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿namespace Sweet.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Resolve Resolve { get; set; }
+
+    }
+    public enum Resolve
+    {
+        Open = 1,
+        Closed
+    }
+}
